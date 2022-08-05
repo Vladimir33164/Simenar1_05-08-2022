26d3810a6709d10b73e02e87c76b9ddd7a3cd75d
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-05 05:16:13 +0300

    Пример входного числа (N), а на выходе все целые числа в промежутке от -N до N.

---
+++ Zadacha005/Program.cs
@@ -0,0 +1,25 @@
+﻿/*5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые 
+числа в промежутке от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+2 -> " -2, -1, 0, 1, 2"*/
+
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number*=-1; // -5 * -1 = 5 - N   
+}
+//-N = N*(-1)
+// [-N ; N] -> [negativeNumber ; number]
+int negativeNumber = number * (-1);
+while (negativeNumber <= number) // Пока справедливо условие
+{
+    Console.WriteLine(negativeNumber);
+    negativeNumber++;
+    //negativeNumber = negativeNumber + 1
+    //Инкремент
+    //negativeNumber--
+    //negativeNumber = negativeNumber - 1
+    //Декремент
+    //negativeNumber +=1
+}
