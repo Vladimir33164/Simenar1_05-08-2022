a7bd0ceddd715c99f62006c99b432acd0f42a83d
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-05 05:06:12 +0300

    Пример для определения дня недели по номеру

---
+++ Zadacha003/Program.cs
@@ -0,0 +1,34 @@
+﻿/*3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+3 -> Среда 
+5 -> Пятница*/
+
+Console.Write("Введите число: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if(dayNumber == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if(dayNumber == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if(dayNumber == 3)
+{
+    Console.WriteLine("Среда");
+}
+if(dayNumber == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if(dayNumber == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if(dayNumber == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if(dayNumber == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
